5787ccb0105c73c9e41664245d0fe945586cfc41
j21winter <j21winter@gmail.com>
2023-07-09 11:56:23 -0400

    OOP & dictionaries

---
+++ fundamentals/fundamentals/basketball_dictionaries/basketball_dictionaries.py
@@ -0,0 +1,109 @@
+players = [
+    {
+        "name": "Kevin Durant", 
+        "age":34, 
+        "position": "small forward", 
+        "team": "Brooklyn Nets"
+    },
+    {
+        "name": "Jason Tatum", 
+        "age":24, 
+        "position": "small forward", 
+        "team": "Boston Celtics"
+    },
+    {
+        "name": "Kyrie Irving", 
+        "age":32, 
+        "position": "Point Guard", 
+        "team": "Brooklyn Nets"
+    },
+    {
+        "name": "Damian Lillard", 
+        "age":33, 
+        "position": "Point Guard", 
+        "team": "Portland Trailblazers"
+    },
+    {
+        "name": "Joel Embiid", 
+        "age":32,
+        "position": "Power Foward", 
+        "team": "Philidelphia 76ers"
+    },
+    {
+        "name": "", 
+        "age":16, 
+        "position": "P", 
+        "team": "en"
+    }
+]
+
+kevin = {
+    "name": "Kevin Durant", 
+    "age":34, 
+    "position": "small forward", 
+    "team": "Brooklyn Nets"
+}
+jason = {
+    "name": "Jason Tatum", 
+    "age":24, 
+    "position": "small forward", 
+    "team": "Boston Celtics"
+}
+kyrie = {
+    "name": "Kyrie Irving", 
+    "age":32, 
+    "position": "Point Guard", 
+    "team": "Brooklyn Nets"
+}
+#! challenge 1
+
+
+
+# player_kevin = Player(kevin)
+# player_jason = Player(jason)
+# player_kyrie = Player(kyrie)
+
+# #! challenge 3
+
+# class Player:
+#     def __init__(self, dict):
+#         self.name = dict['name']
+#         self.age = dict['age']
+#         self.position = dict['position']
+#         self.team = dict['team']
+    
+#     # @classmethod
+#     # def __repr__(self):
+#     #     return '{}'.format(self.name)
+
+# new_team = []
+
+# for dict in players:
+#     player =  Player(dict)
+#     new_team.append(player)
+
+# print(new_team)
+
+# for index_object in new_team:
+#     print(index_object.name)
+
+#TODO ninja Bonus - Not finished..
+
+# TODO class Player:
+#     def __init__(self, dict):
+#         self.name = dict['name']
+#         self.age = dict['age']
+#         self.position = dict['position']
+#         self.team = dict['team']
+
+#     @classmethod
+#     def __repr__(self):
+#         return self.name
+    
+#     @classmethod
+#     def get_team(cls, team_list):
+#         new_list = []
+#         for dict in team_list:
+#             new_list.append(dict)
+#         print(new_list)
+
