c19323a65ccecd4656908b720dc03b3bea5cb6aa
j21winter <j21winter@gmail.com>
2023-07-25 14:42:46 -0400

    CRUD

---
+++ flask_mysql/CRUD/users_cr/server.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, session, redirect, request
+
+from user import User
+
+app = Flask(__name__)
+app.secret_key = 'secret'
+
+@app.route('/read(all)')
+def read_all():
+    users = User.get_all()
+    return render_template('read(all).html', users = users)
+
+@app.route('/create')
+def create():
+    return render_template('create.html')
+
+@app.route('/create_user', methods=['POST'])
+def create_user():
+    data = {
+        'first_name': request.form['first_name'],
+        'last_name': request.form['last_name'], 
+        'email': request.form['email']
+    }
+    User.save(data)
+    return redirect('/read(all)')
+
+if __name__ == '__main__':
+    app.run(debug=True)
