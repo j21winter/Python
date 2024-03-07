ff89990354bbe8912f20ae72f1baa2acc50becbb
j21winter <j21winter@gmail.com>
2024-03-06 21:26:33 -0600

    session

--- Django/djangoIntro/counter/counter/count/urls.py
+++ Django/djangoIntro/counter/counter/count/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('delete', views.delete)
+    path('delete', views.delete),
+    path('double', views.double),
+    path('custom', views.custom)
 ]
--- Django/djangoIntro/counter/counter/count/views.py
+++ Django/djangoIntro/counter/counter/count/views.py
@@ -4,8 +4,10 @@ from django.shortcuts import render, redirect
 def index(request):
     if 'count' in request.session:
         request.session['count'] += 1
+        request.session['custom_count'] += 1
     else: 
         request.session['count'] = 1
+        request.session['custom_count'] = 1
 
     return render(request, 'index.html' )
 
@@ -13,4 +15,25 @@ def delete(request):
     if 'count' in request.session:
         del request.session['count']
     
+    if 'custom_count' in request.session:
+        del request.session['custom_count']
+
+    return redirect('/')
+
+def double(request):
+    if 'custom_count' in request.session:
+        request.session['custom_count'] += 1
+    else: 
+        request.session['custom_count'] = 1
+
+    return redirect('/')
+
+def custom(request):
+    print(request.POST)
+    num = int(request.POST['num'])
+
+    if 'custom_count' in request.session:
+        request.session['custom_count'] += num -1 
+    else: 
+        request.session['custom_count'] = num -1 
     return redirect('/')
